eaccc9bdcf9b4d6cc14a091027062e7153a1796c
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-07-25 22:51:19 +0200

    Figured out how to add the artist along titles

--- index.js
+++ index.js
@@ -29,16 +29,55 @@ const guardians = {
 // Function to generate playlist based on preferred genre
 function generateStarLordPlaylist(guardians, songs) {
 
-    const preferredGenre = guardians["Star-Lord"];
-    const filteredStarLordSongs = songs.filter(song => song.genre === preferredGenre);
-    const playlistStarLord = document.getElementById("playlistStarLord").textContent = filteredStarLordSongs.map(song => song.title).join(", ");
-    playlistStarLord.innerHTML= "Star-Lord's Playlist";
-    console.log("Star-Lord's playlist:", filteredStarLordSongs); 
+const preferedGenre = guardians["Star-Lord"];
+const filteredStarLordSongs = songs.filter(song => song.genre === preferedGenre);// map creates a new array with the filtered song and artist
+starLordSongs = document.getElementById("playlistStarLord").textContent = filteredStarLordSongs.map(song=> `${song.title} by ${song.artist}`).join(", ");
+
+console.log(" Star-Lord's playlist",starLordSongs);
+
+}
+
+function generateGamoraPlayList(guardians,songs){
+const preferedGenre = guardians["Gamora"];
+const filteredGamoraSongs = songs.filter(song => song.genre === preferedGenre);
+document.getElementById("playlistGamora").textContent = filteredGamoraSongs.map(song=> `${song.title} by ${song.artist}`).join(", ");
+console.log("Gamora's playlist" ,filteredGamoraSongs);
+}
+
+function generateDraxPlayList(guardians,songs){
+    const preferedGenre = guardians["Drax"];
+    const filteredDraxSongs = songs.filter(song => song.genre === preferedGenre);
+    document.getElementById("playlistDrax").innerText="Drax's Playlist"
+    document.getElementById("playlistDrax").textContent = filteredDraxSongs.map(song=> `${song.title} by ${song.artist}`).join(", ");
     
+    console.log("Drax's playlist" ,filteredDraxSongs);
     }
+
+function generateRocketPlayList(guardians,songs){
+    const preferedGenre = guardians["Rocket"];
+    const filteredRocketSongs = songs.filter(song => song.genre === preferedGenre);
+    document.getElementById("playlistRocket").textContent = filteredRocketSongs.map(song=> `${song.title} by ${song.artist}`).join(", ");
+    console.log("Rocket's playlist" ,filteredRocketSongs);
+    }
+
+
+function generateGrootPlayList(guardians,songs){
+    const preferedGenre = guardians["Groot"];
+    const filteredGrootSongs = songs.filter(song => song.genre === preferedGenre);
+    document.getElementById("playlistGroot").textContent = filteredGrootSongs.map(song=> `${song.title} by ${song.artist}`).join(", ");
+    console.log("Groot's playlist" ,filteredGrootSongs);
+    }
+
+// I need to figure out why it deletes the name playlist on top , add css styling
+
+
 
 
 // Call generatePlaylist and display the playlists for each Guardian
 generateStarLordPlaylist(guardians, songs);
+generateGamoraPlayList(guardians,songs);
+generateDraxPlayList(guardians,songs);
+generateRocketPlayList(guardians,songs);
+generateGrootPlayList(guardians,songs);
 
 
